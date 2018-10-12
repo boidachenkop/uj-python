f5997d1098b6b2259d1824ad23831d1bdcee5ed8
Pavlo Boidachenko <boidachenkopavlo@gmail.com>
2018-10-12 15:22:15 +0200

    Corrected tasks 2.12 and 2.16

--- prj2/src/zad2-12.py
+++ prj2/src/zad2-12.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # @Author: Pavlo Boidachenko
 # @Date:   2018-10-03 13:21:55
-# @Last Modified by:   pavlo
-# @Last Modified time: 2018-10-03 13:22:28
+# @Last Modified by:   Pavlo Boidachenko
+# @Last Modified time: 2018-10-12 15:18:40
 
 
-S = 'abrakadabra'
-print(S[:3], S[-3:])
+S = 'abcabc xyzxyz kjikji'
+print(" ".join([s[:1] for s in S.split()]))
--- prj2/src/zad2-16.py
+++ prj2/src/zad2-16.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 # @Author: Pavlo Boidachenko
 # @Date:   2018-10-03 17:38:15
-# @Last Modified by:   pavlo
-# @Last Modified time: 2018-10-03 17:42:11
+# @Last Modified by:   Pavlo Boidachenko
+# @Last Modified time: 2018-10-12 15:20:08
 
 
-line  = 'GvR'
-to_add = "uido an ossum"
-print(' '.join([a+b for a, b in zip(line, to_add.split())]))
+line  = 'tekst GvRtekstteksttekst'
+print(line.replace("GvR", "Guido van Rossum"))
